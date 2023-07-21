3c490e1f23f2a280f299ff7ec940878404f8ecbd
Stevie Alvarez <stevie.6strings@gmail.com>
2023-07-21 16:35:34 -0400

    histograms utest: broader traceeval tests
    Test cases of multiple traceeval_type's for the keys and values of a
    traceeval instance, both for equality and inequality, in a variety of
    circumstances.

--- utest/traceeval-utest.c
+++ utest/traceeval-utest.c
@@ -5,21 +5,25 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 
 #include <histograms.h>
 
 #define TRACEEVAL_SUITE		"traceeval library"
+#define TRACEEVAL_SUCCESS	0
+#define TRACEEVAL_NOT_SAME	1
 
 
 /**
- * Test traceeval_init() and traceeval_release() with NULL values.
+ * Test traceeval_init(), traceeval_release(), and traceeval_compare() with
+ * NULL values.
  */
-void test_eval_null()
+void test_eval_null(void)
 {
 	// set up
-	char *name = "test none";
+	char *name = "test null";
 	enum traceeval_data_type type = TRACEEVAL_TYPE_NONE;
 	const struct traceeval_type test_data[] =  {
 		{
@@ -33,7 +37,7 @@ void test_eval_null()
 	struct traceeval *result_key = traceeval_init(test_data, NULL);
 	struct traceeval *result_val = traceeval_init(NULL, test_data);
 	
-	// analyze alloc
+	// analyze init
 	CU_ASSERT(!result_null);
 	CU_ASSERT(!result_key);
 	CU_ASSERT(!result_val);
@@ -46,11 +50,146 @@ void test_eval_null()
 }
 
 /**
+ * Use provided data to test traceeval_init(), traceeval_compare(), and
+ * traceeval_release().
+ */
+void test_eval_base(const struct traceeval_type *keys1,
+		const struct traceeval_type *vals1,
+		const struct traceeval_type *keys2,
+		const struct traceeval_type *vals2,
+		bool init_not_null1, bool init_not_null2,
+		int compare_result, int release_result1, int release_result2)
+{
+	struct traceeval *init1;
+	struct traceeval *init2;
+	int result;
+
+	// test init
+	init1 = traceeval_init(keys1, vals1);
+	init2 = traceeval_init(keys2, vals2);
+
+	result = init1 != NULL;
+	if (init_not_null1) {
+		CU_ASSERT(result);
+	} else {
+		CU_ASSERT(!result);
+	}
+
+	result = init2 != NULL;
+	if (init_not_null2) {
+		CU_ASSERT(result);
+	} else {
+		CU_ASSERT(!result);
+	}
+
+	// test compare
+	result = traceeval_compare(init1, init2);
+	
+	// analyze compare
+	CU_ASSERT(result == compare_result);
+	
+	// test and analyze release
+	CU_ASSERT(traceeval_release(init1) == release_result1);
+	CU_ASSERT(traceeval_release(init2) == release_result2);
+}
+
+/**
+ * Test traceeval_init(), traceeval_release(), and traceeval_compare() with
+ * TRACEEVAL_TYPE_NONE.
+ */
+void test_eval_none(void)
+{
+	// set up
+	char *name = "test none";
+	enum traceeval_data_type type = TRACEEVAL_TYPE_NONE;
+	const struct traceeval_type test_data[] =  {
+		{
+			.type = type,
+			.name = name
+		}
+	};
+	test_eval_base(test_data, test_data, test_data, test_data, true, true,
+			TRACEEVAL_SUCCESS, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+}
+
+/**
+ * Test traceeval_init() and traceeval_release() with equivalent values.
+ */
+void test_eval_same(void)
+{
+	// set up
+	char *name = "test data";
+	char *name2 = "test done";
+	const struct traceeval_type test_data[] =  {
+		{
+			.type = TRACEEVAL_TYPE_NUMBER,
+			.name = name
+		},
+		{
+			.type = TRACEEVAL_TYPE_NONE,
+			.name = name2
+		}
+	};
+	
+	test_eval_base(test_data, test_data, test_data, test_data, true, true,
+			TRACEEVAL_SUCCESS, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+}
+
+/**
+ * Test traceeval_init() and traceeval_release() with non-equivalent values.
+ */
+void test_eval_not_same(void)
+{
+	const struct traceeval_type test_data1[] =  {
+		{
+			.type = TRACEEVAL_TYPE_NUMBER,
+			.name = "test data 1"
+		},
+		{
+			.type = TRACEEVAL_TYPE_NONE,
+			.name = NULL
+		}
+	};
+	const struct traceeval_type test_data2[] =  {
+		{
+			.type = TRACEEVAL_TYPE_NUMBER,
+			.name = "test data 2"
+		},
+		{
+			.type = TRACEEVAL_TYPE_NONE,
+			.name = NULL
+		}
+	};
+
+	// type 1 key diff
+	test_eval_base(test_data2, test_data1, test_data1, test_data1, true,
+			true, TRACEEVAL_NOT_SAME, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+	// type 1 data diff
+	test_eval_base(test_data1, test_data2, test_data1, test_data1, true,
+			true, TRACEEVAL_NOT_SAME, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+	// type 2 key diff
+	test_eval_base(test_data1, test_data1, test_data2, test_data1, true,
+			true, TRACEEVAL_NOT_SAME, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+	// type 2 data diff
+	test_eval_base(test_data1, test_data1, test_data1, test_data2, true,
+			true, TRACEEVAL_NOT_SAME, TRACEEVAL_SUCCESS,
+			TRACEEVAL_SUCCESS);
+}
+
+/**
  * Tests the traceeval_init() and traceeval_release() functions.
  */
 void test_eval(void)
 {
 	test_eval_null();
+	test_eval_none();
+	test_eval_same();
+	test_eval_not_same();
 }
 
 
